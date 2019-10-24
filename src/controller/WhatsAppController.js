a4380afa8d5b4b39bdf297a37f8711d9cf7cfc57
mateusgsouza <mteus.g.souza@gmail.com>
2019-10-23 22:02:07 -0300

    btn message events and record audio

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -9,6 +9,7 @@ class WhatsAppController {
 
     loadElements() {
         //carrega todos os elementos da página e cria os atributos para cada um, formatados no padrão camelCase
+        //ex: id="input-profile-photo" -> this.el.inputProfilePhoto
         this.el = {};
         //recebe todos os elementos com id tratados
 
@@ -19,7 +20,7 @@ class WhatsAppController {
     }
 
     elementsPrototype() {
-        // metodo que adiciona funções reaproveitaveis aos elementos
+        // metodo que adiciona funções reaproveitáveis aos elementos
         Element.prototype.hide = function () {
             this.style.display = 'none';
             return this;//necessario o return para conseguir aninhar funções
@@ -60,6 +61,7 @@ class WhatsAppController {
         Element.prototype.hasClass = function (name) {
             return this.classList.contains(name);
         }
+        
         //funções para formulários
         HTMLFormElement.prototype.getForm = function () {
             return new FormData(this);
@@ -76,15 +78,22 @@ class WhatsAppController {
     }
 
     initEvents() {
-        this.el.myPhoto.on('click', e => { //abre o painel de edição do perfil, fechando os paineis esquerdos que estiverem aberto antes
+        //escuta os eventos do app
+
+        //paineis de perfil e contato
+        this.el.myPhoto.on('click', e => { 
+            //abre o painel de edição do perfil, fechando os paineis esquerdos que estiverem aberto antes
             this.closeAllLeftPanel();
             this.el.panelEditProfile.show();
+
             setTimeout(() => {
                 this.el.panelEditProfile.addClass('open');
-            }, 300);//tempo necessário para carregar a transição do css
+            }, 300);
+            //tempo necessário para carregar a transição do css
 
         });
-        this.el.btnNewContact.on('click', e => { //abre o painel de adicinar contato, fechando os paineis esquerdos que estiverem aberto antes
+        this.el.btnNewContact.on('click', e => {
+             //abre o painel de adicinar contato, fechando os paineis esquerdos que estiverem aberto antes
             this.closeAllLeftPanel();
             this.el.panelAddContact.show();
             setTimeout(() => {
@@ -92,31 +101,42 @@ class WhatsAppController {
             }, 300);
 
         });
-        this.el.btnClosePanelEditProfile.on('click', e => { //fecha o painel de edição de perfil
+
+        //botões de fechar paineis
+        this.el.btnClosePanelEditProfile.on('click', e => { 
+            //fecha o painel de edição de perfil
             this.el.panelEditProfile.removeClass('open');
         });
-        this.el.btnClosePanelAddContact.on('click', e => { //fecha o painel de adicionar contato
+        this.el.btnClosePanelAddContact.on('click', e => { 
+            //fecha o painel de adicionar contato
             this.el.panelAddContact.removeClass('open');
-
         });
+        
+        //inputs de foto de perfil e input de name
         this.el.photoContainerEditProfile.on('click', e => {
+            //abre um input type="file" para seleção de forto de perfil
             this.el.inputProfilePhoto.click();
         });
         this.el.inputNamePanelEditProfile.on('keypress', e => {
+            //recebe o nome digitado para o perfil
             if (e.key === 'Enter') {
                 e.preventDefault();
                 this.el.btnSavePanelEditProfile.click();
             }
         })
+
+        //botões de salvar nome de perfil e contato
         this.el.btnSavePanelEditProfile.on('click', e => {
             console.log(this.el.inputNamePanelEditProfile.innerHTML);
         });
         this.el.formPanelAddContact.on('submit', e => {
             e.preventDefault();
-
             let formData = new FormData(this.el.formPanelAddContact)
         });
+        
+        //conversas
         this.el.contactsMessagesList.querySelectorAll('.contact-item').forEach(item => {
+            //abre a conversa clicada e escode a home
             item.on('click', e => {
                 this.el.home.hide();
                 this.el.main.css({
@@ -124,34 +144,126 @@ class WhatsAppController {
                 });
             });
         });
+
+        //botões de anexar arquivo
         this.el.btnAttach.on('click', e => {
+            //botão que abre o menu de anexos
             e.stopPropagation();
             this.el.menuAttach.addClass('open');
             document.addEventListener('click', this.closeMenuAttach.bind(this));
         });
+
+        //imagens e videos
         this.el.btnAttachPhoto.on('click', e => {
-            console.log('photo')
+            //adicionar imagem/video
+            this.el.inputPhoto.click();
         });
-        this.el.btnAttachCamera.on('click', e => {
-            console.log('Camera')
-        });
-        this.el.btnAttachDocument.on('click', e => {
-            console.log('Document')
-        });
-        this.el.btnAttachContact.on('click', e => {
-            console.log('Contact')
+        this.el.inputPhoto.on('change', e => {
+            //input de imagem/video
+            console.log(this.el.inputPhoto.files);
+            [...this.el.inputPhoto.files].forEach(file => {
+                console.log(file);
+            });
         });
 
+        //câmera
+        this.el.btnAttachCamera.on('click', e => {
+            //anexar foto da câmera
+            this.closeAllMainPanel();
+            this.el.panelCamera.addClass('open');
+            this.el.panelCamera.css({
+                'height': 'calc(100% - 120px)'
+            });
+        });
+        this.el.btnClosePanelCamera.on('click', e => {
+            //fecha o painel da câmera
+            this.closeAllMainPanel();
+            this.el.panelMessagesContainer.show();
+
+        });
+        this.el.btnTakePicture.on('click', e => {
+            //tirar foto
+            console.log('take picture')
+        });
+
+        //documento
+        this.el.btnAttachDocument.on('click', e => {
+            //abre o painel de anexar documento
+            this.closeAllMainPanel();
+            this.el.panelDocumentPreview.addClass('open');
+            this.el.panelDocumentPreview.css({
+                'height': 'calc(100% - 120px)'
+            });
+        });
+        this.el.btnClosePanelDocumentPreview.on('click', e => {
+            //fecha o painel de envio do documentos
+            this.closeAllMainPanel();
+            this.el.panelMessagesContainer.show();
+        });
+        this.el.btnSendDocument.on('click', e => {
+            //botão de enviar o documento
+            console.log('send document');
+        });
+
+        //contatos
+        this.el.btnAttachContact.on('click', e => {
+            //abre o modal de contatos
+            this.el.modalContacts.show();
+        });
+        this.el.btnCloseModalContacts.on('click', e => {
+            //fecha o modal de contatos
+            this.el.modalContacts.hide();
+        });
+
+        //botões de gravação de áudio
+        this.el.btnSendMicrophone.on('click', e => {
+            //esconde o botão de envio de áudio e mostra os botões de gravação
+            this.el.recordMicrophone.show();
+            this.el.btnSendMicrophone.hide();
+            this.startRecordMicrophoneTimer();
+        });
+        this.el.btnCancelMicrophone.on('click', e => {
+            //botão de cancelar a gravação
+            this.closeRecordMicrophone();
+        });
+        this.el.btnFinishMicrophone.on('click', e => {
+            //botão de concluir a gravação
+            this.closeRecordMicrophone();
+        });
+
+
+    }
+
+    startRecordMicrophoneTimer() {
+        //inicia o timer da gravação de áudio
+        let start = Date.now();
+        this._recordMicrophoneInterval = setInterval(() => {
+            this.el.recordMicrophoneTimer.innerHTML = Format.toTime(Date.now() - start);
+        }, 100);//intervalo de atualização do timer
+    }
+
+    closeRecordMicrophone() {
+        //esconde os botões de gravação de áudio  e mostra o botão de envio de áudio
+        this.el.recordMicrophone.hide();
+        this.el.btnSendMicrophone.show();
+        clearInterval(this._recordMicrophoneInterval);//limpa o intervalo de gravação para a próxima gravação inicar zerada
+    }
+
+    closeAllMainPanel() {
+        //fecha os paineis de envio arquivos da conversa
+        this.el.panelMessagesContainer.hide();
+        this.el.panelDocumentPreview.removeClass('open');
+        this.el.panelCamera.removeClass('open');
     }
 
     closeMenuAttach(e) {
+        //fecha o menu de anexos da conversa
         document.removeEventListener('click', this.closeMenuAttach);
-
         this.el.menuAttach.removeClass('open');
-
     }
 
     closeAllLeftPanel() {
+        //fecha os paineis a esqueda
         this.el.panelAddContact.hide();
         this.el.panelEditProfile.hide();
     }
--- src/util/Format.js
+++ src/util/Format.js
@@ -1,12 +1,27 @@
 class Format {
 
     static getCamelCase(text) {
-
+        //recebe as ids, formata e retorna como elemetos que apotam para a id correspondente 
         let div = document.createElement('div');
 
         div.innerHTML = `<div data-${text}="id"></div>`;
 
         return Object.keys(div.firstChild.dataset)[0];
+        
 
     }
+
+    static toTime(duration) {
+        //formata a duração da gravação de áudio
+        let seconds = parseInt((duration / 1000) % 60);
+        let minutes = parseInt((duration / (1000 * 60)) % 60);
+        let hours = parseInt((duration / (1000 * 60 * 60)) % 60);
+
+        if (hours > 0) {
+            return `${hours}:${minutes}:${seconds.toString().padStart(2, '0')}`;
+        } else {
+            return `${minutes}:${seconds.toString().padStart(2, '0')}`;
+        }
+    }
+
 }
